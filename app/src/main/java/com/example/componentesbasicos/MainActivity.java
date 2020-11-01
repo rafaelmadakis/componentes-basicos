e2f737dc08b19d86a0e53948fd5b8259e4be80e8
Rafael Madakis <rafaelmadakis@gmail.com>
2020-11-01 02:09:13 -0300

    Componentes basicos Checkbox

--- app/src/main/java/com/example/componentesbasicos/MainActivity.java
+++ app/src/main/java/com/example/componentesbasicos/MainActivity.java
@@ -4,14 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextInputEditText campoProduto;
     private TextView resultado;
+    private CheckBox cbBranco, cbVerde,cbVermelho;
+    List<String> check = new ArrayList<String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +27,38 @@ public class MainActivity extends AppCompatActivity {
 
         campoProduto = findViewById(R.id.etNomeProduto);
         resultado = findViewById(R.id.tvResultado);
+
+        cbBranco = findViewById(R.id.cbBranco);
+        cbVerde = findViewById(R.id.cbVerde);
+        cbVermelho = findViewById(R.id.cbVermelho);
+    }
+
+    public void verificaCheck(){
+
+        check.clear();
+
+        if (cbBranco.isChecked()) {
+            check.add(cbBranco.getText().toString());
+        }
+
+        if (cbVerde.isChecked()) {
+            check.add(cbVerde.getText().toString());
+        }
+
+        if (cbVermelho.isChecked()) {
+            check.add(cbVermelho.getText().toString());
+        }
+
+        resultado.setText(check.toString());
+
     }
 
     public void btEnviar(View view) {
 
-        String nomeProduto = campoProduto.getText().toString();
-        resultado.setText(nomeProduto);
+//        String nomeProduto = campoProduto.getText().toString();
+//        resultado.setText(nomeProduto);
+
+        verificaCheck();
+
     }
 }
